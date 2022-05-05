8c7adab41926b8f489a196e0e72cc55721401636
Hao Ying <hying5@uci.edu>
2022-05-05 00:35:47 -0700

    Update Index.py

--- Index.py
+++ Index.py
@@ -1,16 +1,29 @@
 import math
 from pathlib import Path
-import re
+# import re
+import os
+import json
+
 
 def findAllUrl(path):
-    entries = Path(path)
-    for entry in entries.iterdir():
-        print(entry)
-        if re.match(r"[0-9a-zA-Z_]*" ,str(entry)):
-            print("  this is path+entry {}".format(str(path) + str(entry)))
-            findAllUrl(path+entry)
-        elif re.match(r"[0-9a-zA-Z_]*\.json",str(entry)):
-            print("  Match Jason")
+    # entries = Path(path)
+    # for entry in entries.iterdir():
+    #     print(entry)
+    #     if re.match(r"[0-9a-zA-Z_]*" ,str(entry)):
+    #         print("  this is path+entry {}".format(str(path) + str(entry)))
+    #         findAllUrl(path+entry)
+    #     elif re.match(r"[0-9a-zA-Z_]*\.json",str(entry)):
+    #         print("  Match Jason")
+
+    j_files = []
+    # add all paths of files end with ".json" into j_files 
+    for r, d, files in os.walk(path):
+        for f in files:
+            if f.endswith('.json'):
+                j_files.append(os.path.join(r, f))
+    # open all files 
+    for j in j_files:
+        info = open(j, "r").read()
 
 
 #CountOfT: count of T in This Doc
