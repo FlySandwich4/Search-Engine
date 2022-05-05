9a5a60ea5235519ac5c723ac58933ad03376161d
Yue Wu <liamwu4@gmail.com>
2022-05-05 14:48:20 -0700

    5.5 afternoon

--- Index.py
+++ Index.py
@@ -1,8 +1,17 @@
 import math
+import urllib.request
 from pathlib import Path
 # import re
 import os
 import json
+from pydoc import Doc
+from bs4 import BeautifulSoup
+import urllib
+
+
+
+print("success import")
+
 
 
 def findAllUrl(path):
@@ -15,15 +24,32 @@ def findAllUrl(path):
     #     elif re.match(r"[0-9a-zA-Z_]*\.json",str(entry)):
     #         print("  Match Jason")
 
-    j_files = []
+    j_files = set()
     # add all paths of files end with ".json" into j_files 
     for r, d, files in os.walk(path):
         for f in files:
             if f.endswith('.json'):
-                j_files.append(os.path.join(r, f))
+                j_files.add(os.path.join(r, f))
     # open all files 
-    for j in j_files:
-        info = open(j, "r").read()
+    return j_files
+        
+
+def BuildIndex(DocSet):
+    Hash_Table = {}
+    DocIndex = 0
+    for eachFile in DocSet:
+        DocIndex += 1
+        data = json.loads(open(eachFile).read())
+        
+        sp = BeautifulSoup(data["content"], "lxml")
+        f = sp.get_text()
+        lst = ['head', 'title', 'h1', 'h2', 'h3']
+        for i in lst:
+            for j in sp.find_all(i):
+                f = f + " " + j.text
+                print("here executed")
+        #(data["content"])
+        #
 
 
 #CountOfT: count of T in This Doc
@@ -35,7 +61,7 @@ def countTFIDF(self, CountOfT, NumOfWords, Documents, TotalOccur):
 
 
 if __name__ =="__main__":
-    findAllUrl('ANALYST/')
+    BuildIndex(findAllUrl('ANALYST/'))
 
     
 
