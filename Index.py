1162b225a13262f052c5ecd2bbb2fc17e017368c
Yue Wu <liamwu4@gmail.com>
2022-05-05 00:02:14 -0700

    AllDocumentSettingUp

--- Index.py
+++ Index.py
@@ -1,1 +1,17 @@
-# Hi hi hi
+import math
+
+def findAllUrl(fileName):
+    pass
+
+#CountOfT: count of T in This Doc
+#NumOfWords: The number of words in this document
+#Documents: The total number of documents
+#TotalOccur: The number of documents that contain T
+def countTFIDF(self, CountOfT, NumOfWords, Documents, TotalOccur): 
+    return (CountOfT/NumOfWords)*math.log(Documents/TotalOccur)
+
+
+    
+
+
+
