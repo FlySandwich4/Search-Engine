634a1145e236ed91423564dd2b39c1a0ff6cfd0e
Yue Wu <liamwu4@gmail.com>
2022-05-05 00:18:57 -0700

    Update Index.py

--- Index.py
+++ Index.py
@@ -1,7 +1,17 @@
 import math
+from pathlib import Path
+import re
 
-def findAllUrl(fileName):
-    pass
+def findAllUrl(path):
+    entries = Path(path)
+    for entry in entries.iterdir():
+        print(entry)
+        if re.match(r"[0-9a-zA-Z_]*" ,str(entry)):
+            print("  this is path+entry {}".format(str(path) + str(entry)))
+            findAllUrl(path+entry)
+        elif re.match(r"[0-9a-zA-Z_]*\.json",str(entry)):
+            print("  Match Jason")
+
 
 #CountOfT: count of T in This Doc
 #NumOfWords: The number of words in this document
@@ -10,6 +20,9 @@ def findAllUrl(fileName):
 def countTFIDF(self, CountOfT, NumOfWords, Documents, TotalOccur): 
     return (CountOfT/NumOfWords)*math.log(Documents/TotalOccur)
 
+
+if __name__ =="__main__":
+    findAllUrl('ANALYST/')
 
     
 
