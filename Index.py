5f31a10b740f8410bc91c4e32483fca83ec11680
Yue Wu <liamwu4@gmail.com>
2022-05-05 17:55:49 -0700

    Almost Done version 5:55 pm

--- Index.py
+++ Index.py
@@ -33,14 +33,16 @@ def findAllUrl(path):
         for f in files:
             if f.endswith('.json'):
                 j_files.add(os.path.join(r, f))
-                
+    global total_doc
     # open all files 
     total_doc = len(j_files)
+    print("This is total {}".format(total_doc))
     return j_files
         
 
 #return : {token: posting}
 def BuildIndex(DocSet):
+    print("build function's toal {}".format(total_doc))
     Hash_Table = {}
     DocIndex = 0
     for eachFile in DocSet:
@@ -61,17 +63,29 @@ def BuildIndex(DocSet):
                 for k in d:
                     if k in Hash_Table:
                         if not Hash_Table[k][-1].docid == DocIndex:
-                            Hash_Table[k].append(Posting(DocIndex, countTFIDF(d[k], Len, total_doc, 6), i))
+                            Hash_Table[k].append(Posting(DocIndex, 0, i, d[k], Len))
                         else:
-                            Hash_Table[k][-1].add_field[i]
+                            Hash_Table[k][-1].add_field(i)
+                            Hash_Table[k][-1].Tokenfre += d[k]
                     else:
                         Hash_Table[k] = []
-                        Hash_Table[k].append(Posting(DocIndex, countTFIDF(d[k], Len, total_doc, 7), i))
+                        Hash_Table[k].append(Posting(DocIndex, 0, i, d[k], Len))
 
                 #i
                 #FileDic {token  : d[1] dic[token][1].append(i)}
+        
+        for token, tokenPostings in Hash_Table.items():
+            TotalOccurOfThisToken = len(tokenPostings)
+            for eachPosting in tokenPostings:
+                eachPosting.tfidf = countTFIDF(eachPosting.Tokenfre, eachPosting.WordsInDocid, total_doc, TotalOccurOfThisToken)
 
-
+        for k,v in Hash_Table.items():
+            print("={}".format(k))
+            for each in v:
+                print(countTF(each.Tokenfre, each.WordsInDocid))
+                #print("    Word fre in total", each.Tokenfre)
+                #print("    Total Words", each.WordsInDocid)
+                print("    DocID",each.docid)
         
         #Tokenizer.Printer(d)
                 #print("here executed")
@@ -85,7 +99,14 @@ def BuildIndex(DocSet):
 #Documents: The total number of documents
 #TotalOccur: The number of documents that contain T
 def countTFIDF(CountOfT, NumOfWords, Documents, TotalOccur): 
-    return (CountOfT/NumOfWords)*math.log(Documents/TotalOccur)
+    #print(Documents)
+    #print("^ doc  v total")
+    #print(TotalOccur)
+
+    return (CountOfT/NumOfWords)*(math.log(Documents/TotalOccur,2))
+
+def countTF(CountOfT, NumOfWords):
+    return (CountOfT/NumOfWords)
 
 
 if __name__ =="__main__":
--- PostingClass.py
+++ PostingClass.py
@@ -1,13 +1,20 @@
 class Posting:
-    def __init__(self,  docid, tfidf, fields):
+    def __init__(self,  docid, tfidf, fields, Tokenfre, WordsInDocid):
         #self.token = token # for a specific word
         self.docid = docid # a url
         self.tfidf = tfidf # use freq counts for now
 
         #fields = []: list contains[ head, p, ...]
         self.fields = [fields]
+
+        #the word frequency appear in this doc
+        self.Tokenfre = Tokenfre
+
+        #the total words in this docid
+        self.WordsInDocid = WordsInDocid
     
     def add_field(self, Str):
         self.fields.append(Str)
 
-    
+    def __str__(self):
+        return "---This posting docid: {d} \n   field: {f}\n   tfidf: {t}".format(d=self.docid, f=self.fields, t=self.tfidf)
