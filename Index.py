53cd6d45828f38c8d2dffaaa2993281785c1501e
Yue Wu <liamwu4@gmail.com>
2022-05-16 18:52:39 -0700

    Update Index.py

--- Index.py
+++ Index.py
@@ -54,126 +54,131 @@ def findAllUrl(path):
     
     return File_lst
         
-
-#return : {token: posting}
-def BuildSmallIndex(DocList,DocIndex):
-    #print("build function's toal {}".format(total_doc))
-    global F_lst
-    global this_time
-    global total_doc
-    Hash_Table = {}
-
+class buildIndex:
+    def __init__(self):
+        self.p_last_index = 0
+        self.h3_last_index = 100000000
+        self.h2_last_index = 200000000
+        self.h1_last_index = 300000000
+        self.title_last_index = 400000000
+        self.head_last_index = 500000000
+    #return : {token: posting}
     
-    for eachFile in DocList:
-        
-        
-        data = json.loads(open(eachFile).read())
-
-        url = data['url']
-        parsed = urlparse(url)
-        new_url = parsed.netloc + parsed.path
-        if new_url not in F_lst:
-            DocIndex += 1
-            F_lst.append(new_url)
-            total_doc += 1
-            this_time += 1
-        
-            sp = BeautifulSoup(data["content"], "lxml")
-
-            WholeTextInEachFile = sp.get_text()
-            # A dictionary containing all tokens' corresponding positions
-            Dict = Tokenizer.READ(WholeTextInEachFile) 
-
-            Len = sum(len(i) for i in Dict.values()) # length of 
-
-            # p: paragraph  h#: small titles 
-            regions = ['p', 'h3', 'h2', 'h1', 'title', 'head']
-            for regionInText in regions:
-
-                for TextInEachRegion in sp.find_all(regionInText):
-                    d = Tokenizer.Count(Tokenizer.READ(TextInEachRegion.text)) 
-                    
-                    for k in d:
-                        if k in Hash_Table:
-                            if not Hash_Table[k][-1].docid == DocIndex:
-                                Hash_Table[k].append(Posting(DocIndex, 0, regionInText, d[k], Len))
-                                if regionInText == 'p' and k in Dict:
-                                    Hash_Table[k][-1].Positions += Dict[k]
-                                elif regionInText == 'h3' and k in Dict:
-                                    Hash_Table[k][-1].Positions += [i + 100000000 for i in Dict[k]]
-                                elif regionInText == 'h2' and k in Dict:
-                                    Hash_Table[k][-1].Positions += [i + 200000000 for i in Dict[k]]
-                                elif regionInText == 'h1' and k in Dict:
-                                    Hash_Table[k][-1].Positions += [i + 300000000 for i in Dict[k]]
-                                elif regionInText == 'title' and k in Dict:
-                                    Hash_Table[k][-1].Positions += [i + 400000000 for i in Dict[k]]
-                                elif regionInText == 'head' and k in Dict:
-                                    Hash_Table[k][-1].Positions += [i + 500000000 for i in Dict[k]]
-                            else:
-                                if regionInText not in Hash_Table[k][-1].fields:
-                                    Hash_Table[k][-1].add_field(regionInText)
-                                Hash_Table[k][-1].Tokenfre += d[k]
-                        else:
-                            Hash_Table[k] = []
-                            Hash_Table[k].append(Posting(DocIndex, 0, regionInText, d[k], Len))
-                            if regionInText == 'p' and k in Dict:
-                                Hash_Table[k][-1].Positions += Dict[k]
-                            elif regionInText == 'h3' and k in Dict:
-                                Hash_Table[k][-1].Positions += [i + 100000000 for i in Dict[k]]
-                            elif regionInText == 'h2' and k in Dict:
-                                Hash_Table[k][-1].Positions += [i + 200000000 for i in Dict[k]]
-                            elif regionInText == 'h1' and k in Dict:
-                                Hash_Table[k][-1].Positions += [i + 300000000 for i in Dict[k]]
-                            elif regionInText == 'title' and k in Dict:
-                                Hash_Table[k][-1].Positions += [i + 400000000 for i in Dict[k]]
-                            elif regionInText == 'head' and k in Dict:
-                                Hash_Table[k][-1].Positions += [i + 500000000 for i in Dict[k]]
-
-
-    for token, tokenPostings in Hash_Table.items():
-        TotalOccurOfThisToken = len(tokenPostings)
-        for eachPosting in tokenPostings:
-            eachPosting.tfidf = countTFIDF(eachPosting.Tokenfre, eachPosting.WordsInDocid, total_doc, TotalOccurOfThisToken)
-
-    return Hash_Table
-
-
-        
-def BuildIndex(D):
-    global jsonNums
-    global this_time
-    Hash_Table = {}
-    n = 0
-    B = []
-    
-    #Breaking Large D into small B List
-    theI = 0
-    while D != []:
-        for i in range(500):
-            try:
-                B.append(D.pop())
-            except:
-                break
-
-        Hash_Table = BuildSmallIndex(B,n)
-
-        with open("AllWords{theI}.json".format(theI = theI), "w+") as F:
-            print("Building Json File:", "AllWords{theI}.json".format(theI = theI))
-            #for token in sorted(Hash_Table.keys()):
-           #     json_obj = json.dumps({token: [i.__dict__ for i in Hash_Table[token]]}, indent=4)
+    def BuildSmallIndex(self, DocList,DocIndex):
+        #print("build function's toal {}".format(total_doc))
+        global F_lst
+        global this_time
+        global total_doc
+        Hash_Table = {}        
+        for eachFile in DocList:            
+            data = json.loads(open(eachFile).read())
+            url = data['url']
+            parsed = urlparse(url)
+            new_url = parsed.netloc + parsed.path
+            if new_url not in F_lst:
+                DocIndex += 1
+                F_lst.append(new_url)
+                total_doc += 1
+                this_time += 1          
+                sp = BeautifulSoup(data["content"], "lxml")
+                              
+                # A dictionary containing all tokens' corresponding positions
+                regions = ['p', 'h3', 'h2', 'h1', 'title', 'head']
+                total_text = ""
+                for r in regions:
+                    for TextInEachRegion in sp.find_all(r): 
+                        total_text += TextInEachRegion.text
+                        total_text += " "
                 
+                Dict = Tokenizer.READ(total_text) 
 
-            json_obj = json.dumps({token: [i.__dict__ for i in Hash_Table[token]] for token in sorted(Hash_Table.keys())}, indent=4 )
-            F.write(json_obj)
+                Len = sum(len(i) for i in Dict.values()) # length of 
 
-        theI += 1
+                # p: paragraph  h#: small titles 
+                
+                for regionInText in regions:
+
+                    for TextInEachRegion in sp.find_all(regionInText):
+                        tokens_position = Tokenizer.READ(TextInEachRegion.text)
+                        d = Tokenizer.Count(tokens_position) 
+                        
+                        for k in d:
+                            lst = tokens_position[k]
+                            if k in Hash_Table:
+                                if not Hash_Table[k][-1].docid == DocIndex:
+                                    Hash_Table[k].append(Posting(DocIndex, 0, regionInText, d[k], Len))
+                                else:
+                                    if regionInText not in Hash_Table[k][-1].fields:
+                                        Hash_Table[k][-1].add_field(regionInText)
+                                    Hash_Table[k][-1].Tokenfre += d[k]
+                            else:
+                                Hash_Table[k] = []
+                                Hash_Table[k].append(Posting(DocIndex, 0, regionInText, d[k], Len))
+
+                            if regionInText == 'p':
+                                lst = [i + self.p_last_index for i in lst]
+                                self.p_last_index = self.p_last_index + len(lst) + 2
+                            elif regionInText == 'h3':
+                                lst = [i + self.h3_last_index for i in lst]
+                                self.h3_last_index = self.h3_last_index + len(lst) + 2
+                            elif regionInText == 'h2':
+                                lst = [i + self.h2_last_index for i in lst]
+                                self.h2_last_index = self.h2_last_index + len(lst) + 2
+                            elif regionInText == 'h1':
+                                lst = [i + self.h1_last_index for i in lst]
+                                self.h1_last_index = self.h1_last_index + len(lst) + 2
+                            elif regionInText == 'title':
+                                lst = [i + self.title_last_index for i in lst]
+                                self.title_last_index = self.title_last_index + len(lst) + 2
+                            elif regionInText == 'head':
+                                lst = [i + self.head_last_index for i in lst]
+                                self.head_last_index = self.head_last_index + len(lst) + 2
+                            Hash_Table[k][-1].Positions += lst
+
+
+        for token, tokenPostings in Hash_Table.items():
+            TotalOccurOfThisToken = len(tokenPostings)
+            for eachPosting in tokenPostings:
+                eachPosting.tfidf = countTFIDF(eachPosting.Tokenfre, eachPosting.WordsInDocid, total_doc, TotalOccurOfThisToken)
+
+        return Hash_Table
+
+            
+    def BuildIndex(self,D):
+        global jsonNums
+        global this_time
         Hash_Table = {}
+        n = 0
         B = []
-        n += this_time
-        this_time = 0
-        #n += 500
-        jsonNums += 1
         
+        #Breaking Large D into small B List
+        theI = 0
+        while D != []:
+            for i in range(500):
+                try:
+                    B.append(D.pop())
+                except:
+                    break
+
+            Hash_Table = self.BuildSmallIndex(B,n)
+
+            with open("AllWords{theI}.json".format(theI = theI), "w+") as F:
+                print("Building Json File:", "AllWords{theI}.json".format(theI = theI))
+                #for token in sorted(Hash_Table.keys()):
+            #     json_obj = json.dumps({token: [i.__dict__ for i in Hash_Table[token]]}, indent=4)
+                    
+
+                json_obj = json.dumps({token: [i.__dict__ for i in Hash_Table[token]] for token in sorted(Hash_Table.keys())}, indent=4 )
+                F.write(json_obj)
+
+            theI += 1
+            Hash_Table = {}
+            B = []
+            n += this_time
+            this_time = 0
+            #n += 500
+            jsonNums += 1
+            
 
 
 def Merge(file_A, file_B):
@@ -251,7 +256,8 @@ def countQueryTFIDF(CountOfT, NumOfWords, Documents, TotalOccur):
     return (1+math.log(CountOfT/NumOfWords, 10))*(math.log(Documents/TotalOccur,10))
 
 if __name__ =="__main__":
-    BuildIndex(findAllUrl('ANALYST/'))
+    BI = buildIndex()
+    BI.BuildIndex(findAllUrl('ANALYST/'))
     with open('url_index.json', 'w+') as F1:
         json_obj = json.dumps(F_lst)
         F1.write(json_obj)
@@ -274,3 +280,7 @@ if __name__ =="__main__":
             File.write(json_obj)
 
     os.remove("AllWords0.json")
+    
+
+
+
